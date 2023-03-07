4c07e06a886d677834cf104bddba196ce3ff1f30
thethinhunao2305 <tsnkazawa@gmail.com>
2023-03-07 17:51:14 +0700

    phu

---
+++ src/main/java/com/example/sbjsp/controller/ProductController.java
@@ -0,0 +1,88 @@
+package com.example.sbjsp.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.sbjsp.entities.Product;
+
+@RestController
+@RequestMapping("product")
+public class ProductController {
+
+	private static List<Product> listproduct = new ArrayList<Product>();
+	private static int i;
+
+	static {
+		listproduct.add(new Product(1, "Xe máy", 1000));
+		listproduct.add(new Product(2, "Ô tô", 120));
+		listproduct.add(new Product(3, "Xe đạp điện", 123123));
+		i = 4;
+	}
+
+	@GetMapping("")
+	public ModelAndView getAllProducts() {
+		ModelAndView mav = new ModelAndView("product/index");
+		mav.addObject("listproduct", listproduct);
+		return mav;
+	}
+
+	@GetMapping("/add")
+	public ModelAndView insert() {
+		ModelAndView mav = new ModelAndView("product/add");
+		return mav;
+	}
+
+	@GetMapping("/edit/{id}")
+	public ModelAndView edit(@PathVariable("id") int id) {
+		ModelAndView mav = new ModelAndView("product/edit");
+		for (Product product : listproduct) {
+			if (product.getId() == id) {
+				mav.addObject("product", product);
+			}
+		}
+		return mav;
+	}
+
+	@PostMapping("/add")
+	public ModelAndView add(@RequestParam("productName") String productName,
+			@RequestParam("productAmount") int productAmount) {
+		listproduct.add(new Product(i++, productName, productAmount));
+		ModelAndView mav = new ModelAndView("redirect:/product");
+		mav.addObject("listproduct", listproduct);
+		return mav;
+	}
+
+	@PostMapping("/edit/{id}")
+	public ModelAndView update(@PathVariable("id") int id, @RequestParam("productName") String productName,
+			@RequestParam("productAmount") int productAmount) {
+		for (Product product : listproduct) {
+			if (product.getId() == id) {
+				product.setProductName(productName);
+				product.setProductAmount(productAmount);
+			}
+		}
+		ModelAndView mav = new ModelAndView("redirect:/product");
+		mav.addObject("listproduct", listproduct);
+		return mav;
+	}
+
+	@GetMapping("/delete/{id}")
+	public ModelAndView delete(@PathVariable("id") int id) {
+		for (Product product : listproduct) {
+			if (product.getId() == id) {
+				listproduct.remove(product);
+			}
+		}
+		ModelAndView mav = new ModelAndView("redirect:/product");
+		mav.addObject("listproduct", listproduct);
+		return mav;
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/product/index.jsp
@@ -0,0 +1,41 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>User</title>
+<link
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+	rel="stylesheet">
+</head>
+<body>
+	<a href="/product/add"><button type="button"
+			class="btn btn-success custom-width">Insert</button></a>
+	<table class="table table-hover table-bordered">
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Product Name</th>
+				<th>Product Amount</th>
+			</tr>
+		</thead>
+		<tbody>
+			<c:forEach items="${listproduct}" var="product">
+				<tr>
+					<td>${product.id}</td>
+					<td>${product.productName}</td>
+					<td>${product.productAmount}</td>
+					<td class="text-center"><a href="/product/edit/${product.id}"><button
+								class="btn btn-primary custom-width">Edit</button></a></td>
+					<td class="text-center"><a
+						href="/product/delete/${product.id}"><button
+								class="btn btn-danger custom-width">Remove</button></a></td>
+				</tr>
+			</c:forEach>
+		</tbody>
+	</table>
+</body>
+</html>
